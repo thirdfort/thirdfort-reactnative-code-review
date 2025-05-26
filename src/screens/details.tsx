51f4f1f90a58563ebb58a6d8073c86a0242b1c95
NuclearRedeye <joel.summerfield@thirdfort.com>
2025-05-26 17:19:44 +0100

    feat: add basic navigation

--- src/screens/details.tsx
+++ src/screens/details.tsx
@@ -1,13 +1,17 @@
+import { useNavigation } from '@react-navigation/native';
 import { View, Text } from 'react-native';
+import { Button } from '@react-navigation/elements';
 
 export type DetailsScreenProps = {
   message?: string;
 };
 
 export function DetailsScreen({message = "Details Screen"}: DetailsScreenProps) {
+  const navigation = useNavigation();
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>{message}</Text>
+      <Button onPress={() => navigation.navigate('Home')}>Go Home</Button>
     </View>
   );
 }
