42b3e705471fe089b2f96139a6c0673b1696c6ee
NuclearRedeye <joel.summerfield@thirdfort.com>
2025-05-27 08:26:42 +0100

    fix: missing styles

---
+++ styles.ts
@@ -0,0 +1,17 @@
+export const typography = {
+  title: {
+    fontSize: 36,
+    lineHeight: 46,
+    fontWeight: 500 as unknown as '500',
+  },
+  secondaryTitle: {
+    fontSize: 30,
+    lineHeight: 40,
+    fontWeight: 500 as unknown as '500',
+  },
+  paragraph: {
+    fontSize: 18,
+    lineHeight: 28,
+    fontWeight: 400 as unknown as '400',
+  }
+}
